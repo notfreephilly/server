9b49da6fcbd210b32b0e32bb7bc3944c785abcd4
notfreephilly <not.philyoder93@gmail.com>
2024-04-10 21:10:21 -0700

    created data.py file and stored catalog with product information

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 import json
 from config import dev, sum
+from data import catalog
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app) #disables CORS policy
 
 @app.get("/") # decorator
 def hello():
@@ -25,6 +27,9 @@ def about():
 ####### API METHODS ############
 ###################################
 
+
+
+
 @app.get('/api/developer')
 def developer():
     return json.dumps(dev)
@@ -34,7 +39,17 @@ def simple_sum():
     ans = sum(21,43)
     return json.dumps(ans)
 
+@app.get('/api/products')
+def get_catalog():
+    return json.dumps(catalog)
 
+
+@app.post("/api/products")
+def save_product():
+    prod = request.get_json() #read the payload of request
+    catalog.append(prod)
+
+    return json.dumps(prod)
 
 
 
